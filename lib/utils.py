1d5e998e74595b0ec7e012a59cbe1fe905acad99
Yongjae <whdydwo8@gmail.com>
2023-02-08 21:16:42 +0900

    Rib waveguide mode simulation

--- lib/utils.py
+++ lib/utils.py
@@ -6,3 +6,8 @@ from enum import Enum, auto, unique, IntEnum
 class BC(IntEnum):
     DBC = 0 # Dirichlet boundary condition
     PBC = 1 # periodic boundary condition
+
+def sparse_div(a, b):
+    inv_b = b.copy()
+    inv_b.data = 1 / inv_b.data
+    return a.multiply(inv_b)
